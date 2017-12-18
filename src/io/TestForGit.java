edeb3fe1c23a1ad1333bf8b9d90a6b8ca4c9e98b
slheluo <875834446@qq.com>
2017-12-18 15:52:58 +0800

    test git!

--- src/io/TestForGit.java
+++ src/io/TestForGit.java
@@ -3,14 +3,14 @@ package io;
 /**
  *
  * @Author sl
- * @Date 2017年12月12日 下午9:59:42
+ * @Date 2017年12月18日 下午3:40:14
  * @Version 1.0
  * 
  */
-public class Practice7 {
+public class TestForGit {
 
 	public static void main(String[] args) {
-		System.out.println("git 7");
+		System.out.println("Hello Git!");
 	}
 
 }
